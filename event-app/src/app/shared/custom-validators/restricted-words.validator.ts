da7137139c078daac744231b0587efdfa9746e81
Kumar Abhishek <kumar.abhishek6@cognizant.com>
2018-05-31 10:54:52 +0530

    event-app/src/app/shared/custom-validators/restricted-words.validator.ts

---
+++ event-app/src/app/shared/custom-validators/restricted-words.validator.ts
@@ -0,0 +1,24 @@
+import { FormControl } from '@angular/forms';
+
+export function restrictedWords(words: string[]) {
+    return (control: FormControl): { [key: string]: any } => {
+      let invalidWords = [];
+      if (words) {
+        invalidWords = words.map(c => {
+          return control.value.includes(c) ? c : null;
+        }).filter(c => c != null);
+
+        if (invalidWords && invalidWords.length > 0) {
+          return { 'restrictedWord': invalidWords.join(',') };
+        }
+      } else {
+        return null;
+      }
+
+      // if (control.value.includes('foo')) {
+      //   return { 'restrictedWord': 'foo' };
+      // } else {
+      //   return null;
+      // }
+    };
+  }
